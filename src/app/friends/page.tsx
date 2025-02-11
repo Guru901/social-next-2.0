295b44ea9ef0ce76f94c26b1ca9a15e19c73c49e
Guru901 <avinashkaur842@gmail.com>
2025-02-11 14:09:59 +0530

    Change it back to client component fixes the issue

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Nav from "@/app/components/Nav";
 import { client } from "@/lib/client";
 import Link from "next/link";
 import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function FriendsPage() {
-  const { friendsData } = await (await client.user.getFriends.$get()).json();
+export default function FriendsPage() {
+  const { data: friendsData } = useQuery({
+    queryKey: ["friends"],
+    queryFn: async () => {
+      const res = await client.user.getFriends.$get();
+      const { friendsData } = await res.json();
+      return friendsData;
+    },
+  });
 
   return (
     <div>

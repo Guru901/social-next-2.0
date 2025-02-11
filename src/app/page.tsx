266ecda4c4d6434a85b52201f85de69ad72e595d
Guru901 <avinashkaur842@gmail.com>
2025-02-11 12:24:12 +0530

    Refactor register page to separate form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,129 +1,13 @@
-"use client";
-
-import { CldUploadWidget } from "next-cloudinary";
-import Link from "next/link";
-import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SignUpSchema } from "@/lib/schemas";
-import { z } from "zod";
-import { client } from "@/lib/client";
-import { useRouter } from "next/navigation";
-
-type SignUpFormData = z.infer<typeof SignUpSchema>;
+import React from "react";
+import RegisterForm from "./form";
 
 export default function Register() {
-  const router = useRouter();
-  const {
-    register,
-    formState: { isSubmitting, errors },
-    handleSubmit,
-    setValue,
-    setError,
-  } = useForm<SignUpFormData>({
-    resolver: zodResolver(SignUpSchema),
-  });
-
-  const [avatar, setAvatar] = useState<string>();
-
-  const onSubmit = handleSubmit(async (data) => {
-    const res = await client.user.register.$post(data);
-    const { success, msg } = await res.json();
-
-    if (success) {
-      router.push("/profile");
-    } else {
-      setError("root", {
-        message: String(msg),
-      });
-    }
-  });
-
   return (
     <div className="flex flex-col w-[100svw] h-[100svh] justify-evenly items-center px-5">
       <div className="flex justify-between">
         <h1 className="text-3xl">Start your Journey</h1>
+        <RegisterForm />
       </div>
-      <form onSubmit={onSubmit} className="flex flex-col w-full max-w-sm gap-3">
-        <div>
-          <input
-            type="text"
-            placeholder="Enter Your Username.."
-            {...register("username")}
-            className="input input-bordered w-full"
-          />
-          {errors.username && (
-            <p className="text-xs text-red-500">{errors.username.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Enter Your Password.."
-            className="input input-bordered w-full"
-            {...register("password")}
-          />
-          {errors.password && (
-            <p className="text-xs text-red-500">{errors.password.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Confirm Password.."
-            className="input input-bordered w-full"
-            {...register("confirmPassword")}
-          />
-          {errors.confirmPassword && (
-            <p className="text-xs text-red-500">
-              {errors.confirmPassword.message}
-            </p>
-          )}
-        </div>
-        <CldUploadWidget
-          uploadPreset="social-nextest"
-          onSuccess={(results) => {
-            // @ts-ignore
-            const secureUrl = results.info.secure_url;
-            setAvatar(secureUrl);
-            setValue("avatar", secureUrl);
-          }}
-        >
-          {({ open }) => {
-            return (
-              <button type="button" onClick={() => open()} className="btn">
-                {avatar ? "Change Avatar" : "Select Avatar"}
-              </button>
-            );
-          }}
-        </CldUploadWidget>
-        {errors.avatar && (
-          <p className="text-xs text-red-500">{errors.avatar.message}</p>
-        )}
-        <div className="flex flex-col text-xs gap-1">
-          <h1>Remember you cant ever change the username</h1>
-          <Link href={"/"} className="underline">
-            Learn more
-          </Link>
-          <Link href={"/login"} className="underline">
-            Already have an account?
-          </Link>
-        </div>
-        {errors.root && (
-          <p className="text-xs text-red-500">{errors.root.message}</p>
-        )}
-        <button type="submit" className="btn" disabled={isSubmitting}>
-          {isSubmitting ? (
-            <>
-              <Loader2 className="animate-spin" />
-              Please wait
-            </>
-          ) : (
-            "Submit"
-          )}
-        </button>
-      </form>
     </div>
   );
 }
